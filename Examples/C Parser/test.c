427e747d862c4f2d2e9fd06e39e2e77f99301453
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2025-11-29 01:22:43 -0800

    Added readme and license, and also fixed float parsing in c example

---
+++ Examples/C Parser/test.c
@@ -0,0 +1,15 @@
+/**
+ * This is for parser example and not to be compiled
+ */
+#include "test.h"
+
+int main()
+{
+    float x = 5.0;
+    float y = .5;
+    float z = 5.7589f;
+    int xI = 5;
+    int yI = 5.0;
+    char *str = "String 1";
+    return 0;
+}
